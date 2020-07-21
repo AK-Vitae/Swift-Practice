5298b47944cb4faf97af5d7bcd24845b7e5c1338
AK <ak.ramadugu@gmail.com>
2020-07-20 21:51:18 -0400

    Added first project that uses UIKit

---
+++ Projects/UIKit/I Am Rich/I Am Rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by Akshith Ramadugu on 7/20/20.
+//  Copyright © 2020 AK-Vitae. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
