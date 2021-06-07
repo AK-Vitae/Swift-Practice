e561bffc1174ae7e4dd463710fcd16b44d82119b
Akshith Ramadugu <aramadugu@webmd.net>
2021-06-07 15:40:15 -0400

    Initial commit for follow along HWS+ portfolio project

---
+++ Projects/SwiftUI/UltimatePortfolio/UltimatePortfolio/UltimatePortfolioApp.swift
@@ -0,0 +1,17 @@
+//
+//  UltimatePortfolioApp.swift
+//  UltimatePortfolio
+//
+//  Created by Ramadugu, Akshith on 7/30/21.
+//
+
+import SwiftUI
+
+@main
+struct UltimatePortfolioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
