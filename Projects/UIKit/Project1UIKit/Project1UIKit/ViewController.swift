dc95b1f88657dbf917eed4edc80f7e84d4bceeaf
Akshith Ramadugu <aramadugu@webmd.net>
2022-01-02 13:09:24 -0500

    UIKit - Project 1: Loaded image content list using FileManager and printed it to console

--- Projects/UIKit/Project1UIKit/Project1UIKit/ViewController.swift
+++ Projects/UIKit/Project1UIKit/Project1UIKit/ViewController.swift
@@ -8,12 +8,22 @@
 import UIKit
 
 class ViewController: UIViewController {
+    var pictures = [String]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let fm = FileManager.default // Lets us work with the filesystem
+        let path = Bundle.main.resourcePath! // A constant path
+        let items = try! fm.contentsOfDirectory(atPath: path) // Gets contents from directory at path
+        
+        for item in items {
+            if item.hasPrefix("nssl") {
+                pictures.append(item)
+            }
+        }
+        
+        print(pictures)
     }
-
-
 }
 
