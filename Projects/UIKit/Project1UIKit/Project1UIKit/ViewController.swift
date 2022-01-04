78eb5e9143b99f63ccb013b12d7069802be00638
Akshith Ramadugu <aramadugu@webmd.net>
2022-01-03 20:27:11 -0500

    UIKit - Project 1: Added functionality to show Detail View when a specific row is tapped

--- Projects/UIKit/Project1UIKit/Project1UIKit/ViewController.swift
+++ Projects/UIKit/Project1UIKit/Project1UIKit/ViewController.swift
@@ -33,7 +33,14 @@ class ViewController: UITableViewController { // Inherits functionality from UIT
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Picture", for: indexPath)
         cell.textLabel?.text = pictures[indexPath.row]
-        return cell 
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if let vc = storyboard?.instantiateViewController(withIdentifier: "Detail") as? DetailViewController {
+            vc.selectedImage = pictures[indexPath.row]
+            navigationController?.pushViewController(vc, animated: true)
+        }
     }
 }
 
