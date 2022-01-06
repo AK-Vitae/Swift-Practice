1417af3750167fb0296b87e3dba87471b47bc3f0
Akshith Ramadugu <aramadugu@webmd.net>
2022-01-06 12:04:10 -0500

    UIKit - Project 5: Initial project set up + implementation of tableView + read string content from file

---
+++ Projects/UIKit/HackingWithSwift/Project5UIKit/Project5UIKit/ViewController.swift
@@ -0,0 +1,47 @@
+//
+//  ViewController.swift
+//  Project5UIKit
+//
+//  Created by Ramadugu, Akshith on 1/6/22.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+    var allWords = [String]()
+    var usedWords = [String()]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        if let startWordsURL = Bundle.main.url(forResource: "start", withExtension: "txt") {
+            if let startWords = try? String(contentsOf: startWordsURL) {
+                allWords = startWords.components(separatedBy: "\n")
+            }
+        }
+        
+        if allWords.isEmpty {
+            allWords = ["silkworm"]
+        }
+        
+        startGame()
+    }
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return usedWords.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Word", for: indexPath)
+        cell.textLabel?.text = usedWords[indexPath.row]
+        return cell
+    }
+    
+    func startGame() {
+        title = allWords.randomElement()
+        usedWords.removeAll(keepingCapacity: true)
+        tableView.reloadData()
+    }
+    
+}
+
