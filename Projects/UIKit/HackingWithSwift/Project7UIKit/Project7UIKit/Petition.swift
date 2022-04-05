d7080969a071d3534d23b9e0461cd129c674df31
Akshith Ramadugu <ak.ramadugu@gmail.com>
2022-04-04 20:44:47 -0400

    UIKit - Project 7: Set up ViewController with tab bar and navigation controller
    Added Petition and Petitions struct that is getting decoded using JSON Decoder

---
+++ Projects/UIKit/HackingWithSwift/Project7UIKit/Project7UIKit/Petition.swift
@@ -0,0 +1,14 @@
+//
+//  Petition.swift
+//  Project7UIKit
+//
+//  Created by Ramadugu, Akshith on 4/4/22.
+//
+
+import Foundation
+
+struct Petition: Codable {
+    var title: String
+    var body: String
+    var signatureCount: Int
+}
---
+++ Projects/UIKit/HackingWithSwift/Project7UIKit/Project7UIKit/Petitions.swift
@@ -0,0 +1,12 @@
+//
+//  Petitions.swift
+//  Project7UIKit
+//
+//  Created by Ramadugu, Akshith on 4/4/22.
+//
+
+import Foundation
+
+struct Petitions: Codable {
+    var results: [Petition]
+}
