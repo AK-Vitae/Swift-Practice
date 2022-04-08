8d9d9e8518fe306a3483a6604f54d91f25497391
Akshith Ramadugu <ak.ramadugu@gmail.com>
2022-04-07 21:13:38 -0400

    UIKit - Milestone 1-3: Added images to Detail View

--- Projects/UIKit/HackingWithSwift/Milestone1-3UIKit/Milestone1-3UIKit/ViewController.swift
+++ Projects/UIKit/HackingWithSwift/Milestone1-3UIKit/Milestone1-3UIKit/ViewController.swift
@@ -47,6 +47,13 @@ class ViewController: UITableViewController {
         cell.textLabel?.text = flags[indexPath.row].uppercased()
         return cell
     }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if let vc = storyboard?.instantiateViewController(withIdentifier: "Detail") as? DetailViewController {
+            vc.selectedImage = flags[indexPath.row]
+            navigationController?.pushViewController(vc, animated: true)
+        }
+    }
 
 }
 
