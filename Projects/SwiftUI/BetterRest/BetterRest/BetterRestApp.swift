99f4fed03d837546eed694595fc5851515ebae50
AK <ak.ramadugu@gmail.com>
2023-12-12 11:42:25 -0500

    SwiftUI iOS 17: BetterRest

---
+++ Projects/SwiftUI/BetterRest/BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Akshith Ramadugu on 12/12/23.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
